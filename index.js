b9e636a9a2f52654411485ce8667f411783297c2
ls1955 <xxlsxx1955@gmail.com>
2023-10-23 20:53:45 +0800

    Include Colors array
    This commit also:
    * Paint the bottom line of canvas with white

---
+++ index.js
@@ -0,0 +1,53 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const Colors = [
+  "#070707",
+  "#1F0707",
+  "#2F0707",
+  "#470F07",
+  "#571707",
+  "#671F07",
+  "#771F07",
+  "#8F2707",
+  "#9F2707",
+  "#AF3F07",
+  "#BF4707",
+  "#C74707",
+  "#DF4707",
+  "#DF5707",
+  "#DF5707",
+  "#D75F07",
+  "#D7670F",
+  "#CF6F0F",
+  "#CF770F",
+  "#CF7F0F",
+  "#CF8717",
+  "#C78717",
+  "#C78F17",
+  "#C7971F",
+  "#BF9F1F",
+  "#BF9F1F",
+  "#BFA727",
+  "#BFA727",
+  "#BFAF2F",
+  "#B7AF2F",
+  "#B7B72F",
+  "#B7B737",
+  "#CFCF6F",
+  "#DFDF97",
+  "#EFEFC7",
+  "#FFFFFF",
+];
+const PixelWidth = 10;
+const PixelHeight = 10;
+
+// Populates bottom canvas with white line, as they are the "fire generator"
+for (let x = 0; x * PixelWidth < canvas.width; x++) {
+    ctx.fillStyle = Colors.at(-1);
+    ctx.fillRect(x * PixelWidth, canvas.height - PixelHeight, PixelWidth, PixelHeight);
+}
+
