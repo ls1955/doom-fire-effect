d482d97d79d7b2cfc9742ac7af5636a8291b4161
ls1955 <xxlsxx1955@gmail.com>
2023-10-24 13:51:05 +0800

    Adjust pixel size
    Adjust pixel size from 5x5 to 7x7.
    
    Because:
    * Improve performance. Less pixels that need to be
      render individually.
    
    This commit also:
    * Clean up some code
    * Include comments

--- index.js
+++ index.js
@@ -5,64 +5,36 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 const Colors = [
-  "#070707",
-  "#1F0707",
-  "#2F0707",
-  "#470F07",
-  "#571707",
-  "#671F07",
-  "#771F07",
-  "#8F2707",
-  "#9F2707",
-  "#AF3F07",
-  "#BF4707",
-  "#C74707",
-  "#DF4707",
-  "#DF5707",
-  "#DF5707",
-  "#D75F07",
-  "#D7670F",
-  "#CF6F0F",
-  "#CF770F",
-  "#CF7F0F",
-  "#CF8717",
-  "#C78717",
-  "#C78F17",
-  "#C7971F",
-  "#BF9F1F",
-  "#BF9F1F",
-  "#BFA727",
-  "#BFA727",
-  "#BFAF2F",
-  "#B7AF2F",
-  "#B7B72F",
-  "#B7B737",
-  "#CFCF6F",
-  "#DFDF97",
-  "#EFEFC7",
-  "#FFFFFF",
+  "#070707", "#1F0707", "#2F0707", "#470F07", "#571707", "#671F07",
+  "#771F07", "#8F2707", "#9F2707", "#AF3F07", "#BF4707", "#C74707",
+  "#DF4707", "#DF5707", "#DF5707", "#D75F07", "#D7670F", "#CF6F0F",
+  "#CF770F", "#CF7F0F", "#CF8717", "#C78717", "#C78F17", "#C7971F",
+  "#BF9F1F", "#BF9F1F", "#BFA727", "#BFA727", "#BFAF2F", "#B7AF2F",
+  "#B7B72F", "#B7B737", "#CFCF6F", "#DFDF97", "#EFEFC7", "#FFFFFF",
 ];
-const PixelWidth = 5;
-const PixelHeight = 5;
-
+const PixelWidth = 7;
+const PixelHeight = 7;
+// A nested array that holds the color index of each pixel
 const colorIndexes = [];
 
+// Populate nested array with black (0)
 for (let y = 0; y * PixelHeight < canvas.height; y++) {
   colorIndexes.push([]);
 
   for (let x = 0; x * PixelWidth < canvas.width; x++) {
-    colorIndexes[colorIndexes.length - 1].push(0);
+    colorIndexes.at(-1).push(0);
   }
 }
+
 // Populate last row with white color, as they are the "fire generator"
 colorIndexes.at(-1).forEach((_, x) => {
-  colorIndexes[colorIndexes.length - 1][x] = Colors.length - 1;
+  colorIndexes.at(-1)[x] = Colors.length - 1;
 });
 
 function drawFires() {
   colorIndexes.forEach((row, y) => {
-    row.forEach((colorIndex, x) => {
-      ctx.fillStyle = Colors[colorIndex];
+    row.forEach((index, x) => {
+      ctx.fillStyle = Colors[index];
       ctx.fillRect(x * PixelWidth, y * PixelHeight, PixelWidth, PixelHeight);
     });
   });
@@ -75,7 +47,6 @@ function spreadFires() {
       let newIndex = colorIndexes[y + 1][x] - (rand & 1);
 
       newIndex = Math.max(0, newIndex);
-
       colorIndexes[y][x] = newIndex;
     }
   }
@@ -84,4 +55,4 @@ function spreadFires() {
 setInterval(() => {
   spreadFires();
   drawFires();
-}, 100);
+}, 1000 / 25);
